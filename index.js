34564217df3e222226c059371dfd799e59cd8021
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2018-05-15 18:24:31 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+let mix = require('laravel-mix');
+let path = require('path');
+let fs = require('fs')
+
+class Prerender {
+    dependencies() {
+        this.requiresReload = true;
+        return [
+            'html-webpack-plugin',
+            'prerender-spa-plugin',
+        ]
+    }
+
+    register({routes, template}) {
+        if(!routes) routes = ['/']
+        if(!template) template = 'resources/views/welcome.blade.php'
+        this.routes = routes
+        this.template = template
+    }
+
+    webpackPlugins() {
+        let HtmlWebpackPlugin = require('html-webpack-plugin');
+        let PrerenderSpaPlugin = require('prerender-spa-plugin');
+        let CopyWebpackPlugin = require('copy-webpack-plugin');
+        return [
+            new HtmlWebpackPlugin({
+                template: this.template,
+                inject: false,
+            }),
+
+            new PrerenderSpaPlugin({
+                staticDir: path.resolve(__dirname, Config.publicPath),
+                routes: this.routes,
+                postProcess: (renderedRoute) => {
+                    
+                    renderedRoute.html = renderedRoute.html.replace("<div id=\"app\"", "<div id=\"app\" server-rendered=\"true\"")
+                    renderedRoute.html = renderedRoute.html.replace("<script>window.__SERVER__=true<script>", "<script>window.__SERVER__=false<script>")
+
+                    let route = renderedRoute.route.replace('/', '')
+                    if(!route) route = 'index'
+                    renderedRoute.outputPath = path.resolve(
+                        __dirname,
+                        path.dirname(this.template),
+                        'rendered', 
+                        route + '.blade.php'
+                    )
+                    return renderedRoute;
+                }
+            }),
+        ]
+    }
+}
+
+mix.extend('prerender', new Prerender());
